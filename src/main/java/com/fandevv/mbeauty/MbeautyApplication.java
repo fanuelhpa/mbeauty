9bb5e4515387321d094aff22b62931f6a073e634
fanuelhpa <fanuelrique@gmail.com>
2024-08-31 20:41:06 -0300

    Project created

---
+++ src/main/java/com/fandevv/mbeauty/MbeautyApplication.java
@@ -0,0 +1,13 @@
+package com.fandevv.mbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fandevv/mbeauty/MbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fandevv.mbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
