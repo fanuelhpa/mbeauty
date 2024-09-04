4f5aa39f05163e07ff2e952a1f060977402e7774
fanuelhpa <fanuelrique@gmail.com>
2024-09-04 14:34:08 -0300

    Scheduling entity and association with User

---
+++ src/main/java/com/fandevv/mbeauty/entities/Scheduling.java
@@ -0,0 +1,91 @@
+package com.fandevv.mbeauty.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_scheduling")
+public class Scheduling implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Instant schedulingDate;
+	private Double price;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public Scheduling() { 
+		
+	}
+
+	public Scheduling(Integer id, Instant schedulingDate, Double price, User user) {
+		super();
+		this.id = id;
+		this.schedulingDate = schedulingDate;
+		this.price = price;
+		this.user = user;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getSchedulingDate() {
+		return schedulingDate;
+	}
+
+	public void setSchedulingDate(Instant schedulingDate) {
+		this.schedulingDate = schedulingDate;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Scheduling other = (Scheduling) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
--- src/main/java/com/fandevv/mbeauty/entities/User.java
+++ src/main/java/com/fandevv/mbeauty/entities/User.java
@@ -1,12 +1,15 @@
 package com.fandevv.mbeauty.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +23,9 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	private String password;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Scheduling> schedulings = new ArrayList<>();
 	
 	public User () {
 	}
@@ -62,6 +68,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Scheduling> getSchedulings() {
+		return schedulings;
 	}
 
 	@Override
